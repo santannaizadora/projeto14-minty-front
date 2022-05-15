57d5073f3308a10fae7896afd75d9ba1b2efc7c2
Lucas Viana <lucasvfv@gmail.com>
2022-05-15 11:51:18 -0300

    fix: overflow in description

--- src/Components/Game/index.js
+++ src/Components/Game/index.js
@@ -77,7 +77,7 @@ const Container = styled.div`
     padding: 89px 14px 25px 14px;
 
     *{
-        font-family: 'Truculenta';
+        font-family: var(--main-font);
     }
 `
 
@@ -131,8 +131,16 @@ const Price = styled.p`
 `
 
 const Description = styled.div`
+
+    height: 140px;
     font-size: 24px;
     line-height: 30px;
+
+    overflow: scroll;
+    ::-webkit-scrollbar {
+    width: 0px;
+}
+
 `
 
 const ButtonCart = styled.button`
