6cb07eeac9a85866b83df6e3baee4f523954fc7c
Lucas Viana <lucasvfv@gmail.com>
2022-05-15 10:42:48 -0300

    feat: search toolbar added

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import TokenContext from "../../contexts/TokenContext";
 import { useState } from "react";
 
 import Login from '../Login';
@@ -8,6 +7,9 @@ import SignUp from "../SignUp"
 import Store from "../Store";
 import Cart from "../Cart";
 import Game from "../Game";
+import Toolbar from "../Toolbar";
+import TokenContext from "../../contexts/TokenContext";
+import SearchContext from "../../contexts/SearchContext";
 
 
 
@@ -18,18 +20,24 @@ export default function App() {
         setToken(localStorage.getItem("token"));
     }
 
+    const [refresh, setRefresh] = useState(false)
+
     return (
         <TokenContext.Provider value={{ token, setToken }}>
-            <BrowserRouter>
-                <ToastContainer />
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/signUp" element={<SignUp />} />
-                    <Route path="/store" element={<Store />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/game/:id" element={<Game />} />
-                </Routes>
-            </BrowserRouter>
+            <SearchContext.Provider value={{ refresh, setRefresh }}>
+                <BrowserRouter>
+                    <ToastContainer />
+                    <Toolbar />
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/signUp" element={<SignUp />} />
+                        <Route path="/store" element={<Store />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/game/:id" element={<Game />} />
+                        <Route path="/search" element={<Toolbar />} />
+                    </Routes>
+                </BrowserRouter>
+            </SearchContext.Provider>
         </TokenContext.Provider>
 
     );
